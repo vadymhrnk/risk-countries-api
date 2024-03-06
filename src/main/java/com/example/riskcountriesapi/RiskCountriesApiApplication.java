c70174fed86d63749b52a45b327df7de8a0c3d52
Vadym Hurnik <vad.com.z@gmail.com>
2024-03-06 12:24:39 +0200

    init commit

---
+++ src/main/java/com/example/riskcountriesapi/RiskCountriesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.riskcountriesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskCountriesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiskCountriesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/riskcountriesapi/RiskCountriesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.riskcountriesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskCountriesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
